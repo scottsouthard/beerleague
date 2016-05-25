fd29876315006ac836aeecf1b3f79b431665fe08
Denny Jovic & Scott Southard <pair+dennyjovic.scottsouthard@devbootcamp.com>
2016-05-25 16:17:46 -0500

    Fix team unless statement issue

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :teams, except: [:index]
   resources :games
   resources :results
-
+  root to: 'leagues#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
