918b10c333f3173fde1e1e3a3cec22ae64da3c8d
Denny Jovic & Scott Southard <pair+dennyjovic.scottsouthard@devbootcamp.com>
2016-05-25 16:59:36 -0500

    Mid scheduling

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -4,8 +4,17 @@ class LeaguesController < ApplicationController
     @teams = Team.all.sort do |a, b|
       b.wins.count <=> a.wins.count
     end
-  end
+    upcoming_games = Game.all.sort do |a, b|
+      b.start_date <=> a.start_date
+    end
+    upcoming_games.map do |game|
+      if game.start_date > DateTime.now
+        game
+      end
+      @upcoming_games = upcoming_games
+    end
 
+  end
 
 
 
