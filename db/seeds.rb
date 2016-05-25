15d10c3cb9691cf20f0f73168d5ae51d58a63d59
Jack Thatcher & Scott Southard <pair+jthatcher96.scottsouthard@devbootcamp.com>
2016-05-25 14:55:21 -0500

    Add newTeam page, update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,18 +4,26 @@ require 'faker'
 
 
 4.times {
-  manager = User.create(full_name: Faker::StarWars.droids, email: Faker::Internet.email, password: 'password')
+  manager = User.create(full_name: Faker::StarWars.droid, email: Faker::Internet.email, password: 'password')
   team = Team.create(name: Faker::Team.name, season_id: 1, manager_id: manager.id)
 
   10.times{
     player = Player.create(name: Faker::StarWars.character, team_id: team.id)
   }
 
-  game = Game.create(location: Faker::StarWars.planet)
+  game = Game.create(location: Faker::StarWars.planet, season_id: 1)
 
   results = Result.create(game_id: game.id, winner_score: rand(6..10), loser_score: rand(1..5))
 
-  2.times {
-    Participant.create(game_id: game.id)
-  }
 }
+
+Season.create(name: "Spring 2016", league_id: 1)
+
+Participant.create(game_id: 1, team_id: 1)
+Participant.create(game_id: 1, team_id: 2)
+Participant.create(game_id: 2, team_id: 3)
+Participant.create(game_id: 2, team_id: 4)
+Participant.create(game_id: 3, team_id: 1)
+Participant.create(game_id: 3, team_id: 3)
+Participant.create(game_id: 4, team_id: 2)
+Participant.create(game_id: 4, team_id: 4)
