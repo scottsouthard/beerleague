f97c5d5f95360fcebbd2559b85390d207578b5b8
Denny Jovic & Scott Southard <pair+dennyjovic.scottsouthard@devbootcamp.com>
2016-05-25 18:27:26 -0500

    Sessions complete

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+  def new
+    render "login"
+  end
+
+  def create
+    @user = User.find_by(params[:email])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to :root
+    else
+      render "login"
+    end
+  end
+
+  def logout
+    session.clear
+    redirect_to :root
+  end
+
+end
